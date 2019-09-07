7674b2f1c1d113decdd9b4f00e272d88c1be6853
Allan Cheng <allanhfcheng@gmail.com>
2019-09-06 19:09:46 -0700

    session

--- src/articles-service.js
+++ src/articles-service.js
@@ -1,9 +1,26 @@
 const ArticlesService = {
 
-    getAllArticles() {
-        return Promise.resolve('all the articles!!')
-           }
-          }
+
+    
+    getAllArticles(knex) {
+        return knex.select('*').from('blogful_articles')
+           
+        },
+          insertArticle(knex, newArticle) {
+            
+                 return knex
+                   .insert(newArticle)
+                   .into('blogful_articles')
+                   .returning('*')
+                   .then(rows => {
+                            return rows[0]
+                           })
+            },
+            getById(knex, id) {
+                   return knex.from('blogful_articles').select('*').where('id', id).first()
+                 },
+
+        }
 
 
 module.exports = ArticlesService
